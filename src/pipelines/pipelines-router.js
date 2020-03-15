42e6010f777656746cb8409bda691225bba7b996
baf62495 <baf62495@aol.com>
2020-03-15 13:52:03 -0400

    pipelines CRUD API endpoints

--- src/pipelines/pipelines-router.js
+++ src/pipelines/pipelines-router.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const logger = require('../../logger');
 const PipelinesService = require('./pipelines-service');
+const path = require('path');
 const xss = require('xss');
 
 const pipelinesRouter = express.Router();
 const jsonParser = express.json();
+
+const serializePipeline = pipeline => ({
+  id: pipeline.id,
+  title: xss(pipeline.title),
+  team_id: pipeline.team_id
+});
 
 pipelinesRouter
   .route('/')
   .get((req, res, next) => {
     PipelinesService.getAllPipelines(req.app.get('db'))
       .then(pipelines => {
-        res.json(pipelines);
+        res.json(pipelines.map(serializePipeline));
       })
       .catch(next);
   })
-  .post(() => {});
+  .post(jsonParser, (req, res, next) => {
+    const { title, team_id } = req.body;
+    const newPipeline = { title, team_id };
+
+    for (const [key, value] of Object.entries(newPipeline))
+      if (value == null) {
+        logger.error(`Missing '${key}' in request body`);
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        });
+      }
+
+    PipelinesService.insertPipeline(req.app.get('db'), newPipeline)
+      .then(pipeline => {
+        logger.info(`Pipeline with id ${pipeline.id} created`);
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${pipeline.id}`))
+          .json(serializePipeline(pipeline));
+      })
+      .catch(next);
+  });
 
 pipelinesRouter
   .route('/:id')
@@ -35,7 +63,41 @@ pipelinesRouter
       .catch(next);
   })
   .get((req, res) => {
-    res.json(res.pipeline);
+    res.json(serializePipeline(res.pipeline));
+  })
+  .delete((req, res, next) => {
+    const { id } = req.params;
+    PipelinesService.deletePipeline(req.app.get('db'), id)
+      .then(() => {
+        logger.info(`Pipeline with id ${id} deleted`);
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, team_id } = req.body;
+    const pipelineToUpdate = { title, team_id };
+
+    const numberOfValues = Object.values(pipelineToUpdate).filter(Boolean)
+      .length;
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title' or 'team_id'`
+        }
+      });
+    }
+
+    PipelinesService.updatePipeline(
+      req.app.get('db'),
+      req.params.id,
+      serializePipeline(pipelineToUpdate)
+    )
+      .then(numRowsAffected => {
+        logger.info(`Pipeline with id ${req.params.id} updated`);
+        res.status(204).end();
+      })
+      .catch(next);
   });
 
 module.exports = pipelinesRouter;
--- test/pipelines-endpoints.spec.js
+++ test/pipelines-endpoints.spec.js
@@ -69,54 +69,254 @@ describe.only('Pipelines Endpoints', function() {
           .expect(200, testPipelines);
       });
     });
+  });
 
-    describe(`GET /api/pipelines/:id`, () => {
-      context('Given no pipelines', () => {
-        it('responds with a 404', () => {
-          const id = 12345;
-          return supertest(app)
-            .get(`/api/pipelines/${id}`)
-            .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-            .expect(404, {
-              error: { message: 'Pipeline not found' }
-            });
-        });
+  describe('GET /api/pipelines/:id', () => {
+    context('Given no pipelines', () => {
+      it('responds with a 404', () => {
+        const id = 12345;
+        return supertest(app)
+          .get(`/api/pipelines/${id}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, {
+            error: { message: 'Pipeline not found' }
+          });
+      });
+    });
+
+    context('Given there are pipelines in the database', () => {
+      const testTeams = makeTeamsArray();
+      const testUserRoles = makeUserRolesArray();
+      const testUsers = makeUsersArray();
+      const testPipelines = makePipelinesArray();
+
+      beforeEach('insert pipelines', () => {
+        return db
+          .into('teams')
+          .insert(testTeams)
+          .then(() => {
+            return db
+              .into('user_roles')
+              .insert(testUserRoles)
+              .then(() => {
+                return db
+                  .into('users')
+                  .insert(testUsers)
+                  .then(() => {
+                    return db.into('pipelines').insert(testPipelines);
+                  });
+              });
+          });
       });
 
-      context('Given there are pipelines in the database', () => {
-        const testTeams = makeTeamsArray();
-        const testUserRoles = makeUserRolesArray();
-        const testUsers = makeUsersArray();
-        const testPipelines = makePipelinesArray();
+      it('responds with a 200 and the pipeline with id', () => {
+        const idToGet = 2;
+        const expectedPipeline = testPipelines[idToGet - 1];
 
-        beforeEach('insert pipelines', () => {
+        return supertest(app)
+          .get(`/api/pipelines/${idToGet}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, expectedPipeline);
+      });
+    });
+  });
+
+  describe('POST /api/pipelines', () => {
+    const testTeams = makeTeamsArray();
+    const testUserRoles = makeUserRolesArray();
+    const testUsers = makeUsersArray();
+
+    beforeEach('insert users', () => {
+      return db
+        .into('teams')
+        .insert(testTeams)
+        .then(() => {
           return db
-            .into('teams')
-            .insert(testTeams)
+            .into('user_roles')
+            .insert(testUserRoles)
             .then(() => {
-              return db
-                .into('user_roles')
-                .insert(testUserRoles)
-                .then(() => {
-                  return db
-                    .into('users')
-                    .insert(testUsers)
-                    .then(() => {
-                      return db.into('pipelines').insert(testPipelines);
-                    });
-                });
+              return db.into('users').insert(testUsers);
             });
         });
+    });
 
-        it('responds with a 200 and the pipeline with id', () => {
-          const idToGet = 2;
-          const expectedPipeline = testPipelines[idToGet - 1];
-
-          return supertest(app)
-            .get(`/api/pipelines/${idToGet}`)
+    it('creates a pipelines, responding with 201 and the new pipeline', () => {
+      const newPipeline = {
+        title: 'Test Pipeline',
+        team_id: 1
+      };
+      return supertest(app)
+        .post('/api/pipelines')
+        .send(newPipeline)
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.title).to.eql(newPipeline.title);
+          expect(res.body.team_id).to.eql(newPipeline.team_id);
+          expect(res.body).to.have.property('id');
+          expect(res.header.location).to.eql(`/api/pipelines/${res.body.id}`);
+        })
+        .then(postRes =>
+          supertest(app)
+            .get(`/api/pipelines/${postRes.body.id}`)
             .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-            .expect(200, expectedPipeline);
-        });
+            .expect(postRes.body)
+        );
+    });
+
+    const requiredFields = ['title', 'team_id'];
+
+    requiredFields.forEach(field => {
+      const newPipeline = {
+        title: 'Test Pipeline',
+        team_id: 1
+      };
+
+      it('responds with 400 and an error when the required field is missing', () => {
+        delete newPipeline[field];
+
+        return supertest(app)
+          .post('/api/pipelines')
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(newPipeline)
+          .expect(400, {
+            error: { message: `Missing '${field}' in request body` }
+          });
+      });
+    });
+  });
+
+  describe('DELETE /api/pipelines/:id', () => {
+    context('given no pipelines', () => {
+      it('responds with 404', () => {
+        const id = 12345;
+        return supertest(app)
+          .delete(`/api/pipelines/${id}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, {
+            error: { message: 'Pipeline not found' }
+          });
+      });
+    });
+
+    context('Given there are pipelines in the database', () => {
+      const testTeams = makeTeamsArray();
+      const testUserRoles = makeUserRolesArray();
+      const testUsers = makeUsersArray();
+      const testPipelines = makePipelinesArray();
+
+      beforeEach('insert pipelines', () => {
+        return db
+          .into('teams')
+          .insert(testTeams)
+          .then(() => {
+            return db
+              .into('user_roles')
+              .insert(testUserRoles)
+              .then(() => {
+                return db
+                  .into('users')
+                  .insert(testUsers)
+                  .then(() => {
+                    return db.into('pipelines').insert(testPipelines);
+                  });
+              });
+          });
+      });
+
+      it('responds with 204 and removes the pipeline', () => {
+        const idToDelete = 2;
+        const expectedPipelines = testPipelines.filter(
+          pipeline => pipeline.id !== idToDelete
+        );
+
+        return supertest(app)
+          .delete(`/api/pipelines/${idToDelete}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get('/api/pipelines')
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedPipelines)
+          );
+      });
+    });
+  });
+
+  describe('PATCH /api/pipelines/:id', () => {
+    context('given no pipelines', () => {
+      it('responds with 404', () => {
+        const id = 12345;
+        return supertest(app)
+          .patch(`/api/pipelines/${id}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .expect(404, {
+            error: { message: 'Pipeline not found' }
+          });
+      });
+    });
+
+    context('Given there are pipelines in the database', () => {
+      const testTeams = makeTeamsArray();
+      const testUserRoles = makeUserRolesArray();
+      const testUsers = makeUsersArray();
+      const testPipelines = makePipelinesArray();
+
+      beforeEach('insert pipelines', () => {
+        return db
+          .into('teams')
+          .insert(testTeams)
+          .then(() => {
+            return db
+              .into('user_roles')
+              .insert(testUserRoles)
+              .then(() => {
+                return db
+                  .into('users')
+                  .insert(testUsers)
+                  .then(() => {
+                    return db.into('pipelines').insert(testPipelines);
+                  });
+              });
+          });
+      });
+
+      it('responds with 204 and updates the pipeline', () => {
+        const idToUpdate = 2;
+        const updatePipeline = {
+          title: 'PATCH test pipeline',
+          team_id: 1
+        };
+        const expectedPipeline = {
+          ...testPipelines[idToUpdate - 1],
+          ...updatePipeline
+        };
+
+        return supertest(app)
+          .patch(`/api/pipelines/${idToUpdate}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(updatePipeline)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/pipelines/${idToUpdate}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedPipeline)
+          );
+      });
+
+      it(`responds with 400 when no required fields supplied`, () => {
+        const idToUpdate = 2;
+        return supertest(app)
+          .patch(`/api/pipelines/${idToUpdate}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send({ irrelevantField: 'foo' })
+          .expect(400, {
+            error: {
+              message: `Request body must contain either 'title' or 'team_id'`
+            }
+          });
       });
     });
   });
